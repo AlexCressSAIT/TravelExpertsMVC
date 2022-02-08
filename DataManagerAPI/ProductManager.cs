be303d423463ca09600430060a28f07dd45fa751
Nate Penner <natepenner@gmail.com>
2022-02-08 15:16:18 -0700

    Setup DataManagerAPI project
    Set up the project for the business classes. Added reference to TravelExpertsData

---
+++ DataManagerAPI/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelExpertsData;
+
+namespace DataManagerAPI
+{
+    public static class ProductManager
+    {
+        public static List<Product> GetProducts()
+        {
+            TravelExpertsContext db = new TravelExpertsContext();
+
+            return db.Products.ToList();
+        }
+    }
+}
