5b8232eabd4ffe6c27e69154ac80f317eb8c0b17
Nate Penner <natepenner@gmail.com>
2022-02-08 15:19:30 -0700

    Added a demo products page
    Added a demo list Product controller and view to test that everything is working correctly.

---
+++ TravelExperts/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using DataManagerAPI;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelExperts.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View(ProductManager.GetProducts());
+        }
+    }
+}
