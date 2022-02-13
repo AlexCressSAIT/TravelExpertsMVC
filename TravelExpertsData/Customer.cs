3003b3a1e91fdc83aadb0e7768fefee8027e3beb
Daniel Palmer <daniel.palmer@shaw.ca>
2022-02-12 18:16:33 -0700

    Created Register/Edit Customer View

--- TravelExpertsData/Customer.cs
+++ TravelExpertsData/Customer.cs
@@ -21,35 +21,35 @@ namespace TravelExpertsData
         [Key]
         [Display(Name = "Account No.")]
         public int CustomerId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Firstname is required")]
         [StringLength(25)]
         [Display(Name = "First Name")]
         public string CustFirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Lastname is required")]
         [StringLength(25)]
         [Display(Name = "Last Name")]
         public string CustLastName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Address is required")]
         [StringLength(75)]
         [Display(Name = "Address")]
         public string CustAddress { get; set; }
-        [Required]
+        [Required(ErrorMessage = "City is required")]
         [StringLength(50)]
         [Display(Name = "City")]
         public string CustCity { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Province is required")]
         [StringLength(2)]
         [Display(Name = "Provice/State")]
         public string CustProv { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Postal code is required")]
         [StringLength(7)]
         [Display(Name = "Postal/Zip Code")]
         public string CustPostal { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Country is required")]
         [StringLength(25)]
         [Display(Name = "Country")]
         public string CustCountry { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Phone Number is required")]
         [StringLength(20)]
         [Display(Name = "Home Phone")]
         public string CustHomePhone { get; set; }
@@ -60,14 +60,17 @@ namespace TravelExpertsData
         [Display(Name = "Email")]
         public string CustEmail { get; set; }
         public int? AgentId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
         [StringLength(50)]
         [Display(Name = "Username")]
         public string CustUsername { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
         [StringLength(50)]
         [Display(Name = "Password")]
         public string CustPassword { get; set; }
+        [Display(Name="Confirm Password")]
+        [Compare("CustPassword", ErrorMessage = "Confirm password doesn't match, Type again !")]
+        public string ConfirmPassword { get; set; }
 
         [ForeignKey(nameof(AgentId))]
         [InverseProperty("Customers")]
