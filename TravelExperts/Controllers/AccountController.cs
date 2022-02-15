a495d3adeb526b4b19a7e0cf854fce50c136a3ab
Nate Penner <natepenner@gmail.com>
2022-02-15 14:14:59 -0700

    Login page fields are now validated

--- TravelExperts/Controllers/AccountController.cs
+++ TravelExperts/Controllers/AccountController.cs
@@ -3,10 +3,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using TravelExpertsData;
@@ -28,6 +26,12 @@ namespace TravelExperts.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(Customer customer)
         {
+            if (ModelState["CustUsername"].Errors.Count > 0 
+                || ModelState["CustPassword"].Errors.Count > 0)
+            {
+                ViewBag.LoginError = "Please fill out all fields.";
+                return View();
+            }
             Customer loginCustomer = CustomerManager.Authenticate(customer.CustUsername, customer.CustPassword);
             if (loginCustomer == null)
             {
